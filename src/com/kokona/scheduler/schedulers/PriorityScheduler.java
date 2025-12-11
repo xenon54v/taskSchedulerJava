0f0630132d27a4b8cc066a9ccfec382e3fb4c45d
xenon54v <kokona955@gmail.com>
2025-12-11 23:49:42 +0300

    создан планировщик приоритетов

--- src/com/kokona/scheduler/schedulers/PriorityScheduler.java
+++ src/com/kokona/scheduler/schedulers/PriorityScheduler.java
@@ -1,5 +1,14 @@
 package com.kokona.scheduler.schedulers;
 
-public class PriorityScheduler {
+import com.kokona.scheduler.model.Task;
+import com.kokona.scheduler.datastructures.MinHeap;
+import java.util.Comparator;
+
+public class PriorityScheduler extends Scheduler {
+	private MinHeap<Task> heap;
+	
+	public PriorityScheduler() {
+		heap = new MinHeap<>(Comparator.comparingInt(Task::getPriority).reversed());
+	}
 
 }
